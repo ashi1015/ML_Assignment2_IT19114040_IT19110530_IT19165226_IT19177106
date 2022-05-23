5f06ae5da848bf4452a8a489ded41d47318e7efb
JayakodyJADKA <it19114040@my.sliit.lk>
2022-05-23 13:13:47 +0530

    Counting categorical & nueric cols

--- Decision_Tree_Implementation.py
+++ Decision_Tree_Implementation.py
@@ -12,3 +12,14 @@ import numpy as np
 # Importing the dataset
 training_df = pd.read_csv('train.csv')
 training_df.info()
+
+# Counting the number of numerical columns and categorical columns
+
+# Drop the Load ID column in the dataset
+training_df = training_df.drop(columns=['Loan_ID']) 
+
+numerical_data_col_array = ['ApplicantIncome', 'CoapplicantIncome', 'LoanAmount']
+print(numerical_data_col_array)
+
+categorical_data_col_array = ['Gender', 'Married', 'Dependents', 'Education', 'Self_Employed', 'Property_Area','Credit_History','Loan_Amount_Term']
+print(categorical_data_col_array)
