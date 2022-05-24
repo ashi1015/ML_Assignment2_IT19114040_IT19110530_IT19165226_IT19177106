f3856e8db50e64af2bd10f145a67a57e4a2fa55e
JayakodyJADKA <it19114040@my.sliit.lk>
2022-05-24 13:19:28 +0530

    Split dataset to test & train data

--- Decision_Tree_Implementation.py
+++ Decision_Tree_Implementation.py
@@ -57,3 +57,7 @@ x_val = encoded_training_df.drop(columns='Loan_Status_Y')
 # Assign the Load_Status_Y column to y_val
 y_val = encoded_training_df['Loan_Status_Y']
 
+# Splitting the datset into training and testing data
+X_training, X_testing, y_training, y_testing = train_test_split(x_val, y_val, test_size = 0.2, stratify = y_val, 
+                                                                random_state = 42)
+																
