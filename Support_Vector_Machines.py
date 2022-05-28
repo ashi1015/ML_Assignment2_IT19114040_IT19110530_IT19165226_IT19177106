c92ca7731093460e0358da7e35632b673714c6c1
IT19177106 <it19177106@my.sliit.lk>
2022-05-28 20:03:00 +0530

    Data Labeling,Checking Data,Separating Data and Label,Spliting Data,Checking the Shape,Checking the Shape,Classifying the data,Training data of the data set

--- Support_Vector_Machines.py
+++ Support_Vector_Machines.py
@@ -61,3 +61,34 @@ for idx,cat_col in enumerate(numerical_columns):
 
 print(loan_tarin_df4[numerical_columns].describe())
 plt.subplots_adjust(hspace=1)
+
+# Data Labeling
+loan_tarin_df4.replace({"Loan_Status": {'N': 0, 'Y': 1}}, inplace=True)
+loan_tarin_df4.replace({"Married": {'No': 0, 'Yes': 1}}, inplace=True)
+loan_tarin_df4.replace({"Self_Employed": {'No': 0, 'Yes': 1}}, inplace=True)
+loan_tarin_df4.replace({"Education": {'Not Graduate': 0, 'Graduate': 1}}, inplace=True)
+loan_tarin_df4.replace({"Gender": {'Male': 0, 'Female': 1}}, inplace=True)
+loan_tarin_df4.replace({"Property_Area": {'Rural': 0, 'Semiurban': 1, 'Urban': 2}}, inplace=True)
+
+# Checking Data
+loan_tarin_df4.head()
+
+# Separating Data and Label
+X = loan_tarin_df4.drop(columns=['Loan_ID', 'Loan_Status'], axis = 1)
+Y = loan_tarin_df4['Loan_Status']
+X, Y
+
+# Spliting Data
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, stratify = Y, random_state = 3)
+
+# Checking the Shape
+X.shape, X_train.shape, X_test.shape
+
+# Checking the Shape
+Y.shape, Y_train.shape, Y_test.shape
+
+# Classifying the data 
+classifier = svm.SVC(kernel = 'linear')
+
+# Training data of the data set
+classifier.fit(X_train, Y_train)
